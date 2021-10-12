0b8fa95951029a616451cb59395555952015169e
Jason Hargrove <jasonhargrove1@gmail.com>
2021-10-12 00:48:29 -0700

    simplify nav and finish responsive styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,76 +1,62 @@
 import "./Header.scss";
+import { Link } from "gatsby";
 import * as React from "react";
-import { FaBars,FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import companyLogo from "../images/logo.png";
-import { useEffect,useState } from "react";
-import { IconContext } from "react-icons/lib";
 import {
-  MobileIcon,
-  Nav,
-  NavbarContainer,
-  NavItem,
-  NavLinks,
-  NavLogo,
-  NavMenu,
-  NavBtn
-} from "./HeaderNav";
+  // useEffect,
+  useState
+} from "react";
+import { IconContext } from "react-icons/lib";
 
 const Header = () => {
-  const [click,setClick] = useState(false);
-  const [scroll,setScroll] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  // const [scroll,setScroll] = useState(false);
 
-  const handleClick = ()=> setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setShowMobileMenu(!showMobileMenu);
+  const handleKeyDown = (e) => (e.keyCode === 13) ? setShowMobileMenu(!showMobileMenu) : null;
 
-  const changeNav = () => {
-    if (window.scrollY >= 80){
-      setScroll(true);
-    } else{
-      setScroll(false);
-    }
-  };
+  // const closeMobileMenu = () => setClick(false);
 
-  useEffect(()=>{
-    changeNav();
-    window.addEventListener("scroll", changeNav);
-  },[]);
+  // const changeNav = () => {
+  //   if (window.scrollY >= 80){
+  //     setScroll(true);
+  //   } else{
+  //     setScroll(false);
+  //   }
+  // };
+
+  // useEffect(()=>{
+  //   // changeNav();
+  //   // window.addEventListener("scroll", changeNav);
+  // },[showMobileMenu]);
 
   return(
-    <>
+    <div className="header">
       <IconContext.Provider value={{ color: "#131313"}}>
-      <Nav active={scroll} click={click}  >
-      
-      <NavbarContainer >
-        <NavLogo to="/" onClick={closeMobileMenu}>
-        <img src ={companyLogo} alt="logo"/> 
-      
-        </NavLogo>
-        <MobileIcon onClick={handleClick}>
-          {click ?<FaTimes/>:<FaBars/>}
-        </MobileIcon>
-        <NavMenu  onClick={handleClick} click={click}>
-
-     
-        <NavItem>
-          <NavLinks to="/">Markets</NavLinks>
-        </NavItem>
-        <NavItem>
-          <NavLinks to="/">Portfolio</NavLinks>
-        </NavItem>
-        <NavItem>
-          <NavLinks to="/">Faucets</NavLinks>
-        </NavItem>
-        <NavItem>
-          <NavLinks to="/">Docs</NavLinks>
-        </NavItem>
-        <NavBtn>
-          Launch app
-        </NavBtn>
-        </NavMenu>
-      </NavbarContainer>
-      </Nav>
-    </IconContext.Provider >
-    </>
+        <nav className={`header-nav ${(showMobileMenu)? 'show-nav' : 'hide-nav'}`}>
+          <div className="header-logo">
+            <Link to="/404"><img src={companyLogo} alt="PsyOptions Logo Icon" /></Link>
+          </div>
+          <ul>
+            <li><Link to="/404">Markets</Link></li>
+            <li><Link to="/404">Portfolio</Link></li>
+            <li><Link to="/404">Faucets</Link></li>
+            <li><Link to="/404">Docs</Link></li>
+          </ul>
+          <div className="header-mobile-icon"
+               onClick={handleClick}
+               onKeyDown={handleKeyDown}
+               tabIndex="0"
+               role="button">
+            {showMobileMenu ?<FaTimes/>:<FaBars/>}
+          </div>
+          <div className="header-nav-button">
+            <Link to="/404">Launch App</Link>
+          </div>
+        </nav>
+      </IconContext.Provider >
+    </div>
   )
 };
 
--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,135 +1,173 @@
-import { Link } from "gatsby";
-import styled from "styled-components";
+// import { Link } from "gatsby";
+// import styled from "styled-components";
 
-export const Nav =styled.nav `
-  background-color: transparent;
-  height:80px;
-  display:flex;
-  font-size:1rem;
-  font-family: 'Manrope', sans-serif;   
-  position:sticky !important;
-  top:0;
-  z-index:999;
-  width:100%;
+// export const Nav =styled.nav `
+//   background: black;
+//   bottom: 0;
+//   display: flex;
+//   flex-flow: column;
+//   left: 0;
+//   position: absolute;
+//   right: 0;
+//   top: 0;
+//   z-index:999;
 
-  @media screen and (max-width:425px){
-    transition:0.8s all ease;
-    position:sticky !important;
-    width:100% !important;
-  }
-`
-export const NavbarContainer=styled.div`
-  display:flex;
-  align-items:center;
-  justify-content:space-between;
-  height:80px;
-  z-index:1;
-  width:100%;
-  max-width:100%;
-  @media screen and (max-width:425px){
-    position:sticky !important;
-    width:100% !important;
-  }
-`
+//   // background-color: transparent;
+//   // height:80px;
+//   // display:flex;
+//   // font-size:1rem;
+//   // font-family: 'Manrope', sans-serif;   
+//   // position:sticky !important;
+//   // top:0;
+//   // width:100%;
 
-export const NavLogo = styled(Link)`
-  align-items:left;
-  padding:1rem 0rem;
-  color:white;
-  justify-self:self-start;
-  margin-left:3rem !important;
-`
+//   // @media screen and (max-width:425px){
+//   //   transition:0.8s all ease;
+//   //   position:sticky !important;
+//   //   width:100% !important;
+//   // }
+// `
+// // export const NavbarContainer=styled.div`
+// //   // border: 2px dotted yellow;
+// //   // position: relative;
+// //   // width: 100%;
+// //   // display:flex;
+// //   // align-items:center;
+// //   // justify-content:space-between;
+// //   // height:80px;
+// //   // z-index:1;
+// //   // width:100%;
+// //   // max-width:100%;
+// //   // @media screen and (max-width:425px){
+// //   //   position:sticky !important;
+// //   //   width:100% !important;
+// //   // }
+// // `
 
-export const MobileIcon= styled.div`
-  display:none;
+// export const NavLogo = styled(Link)`
+//   // align-items:left;
+//   // padding: 1rem 0rem;
+//   // color:white;
+//   // justify-self:self-start;
+//   // position: absolute;
+//   // left: 10px;
+//   // top: 13;
 
-  @media screen and (max-width:425px){
-    display:block;
-    position:absolute;
-    top:0;
-    right:0 ;
-    transform:translate(-100%,60%);
-    font-size:2.5rem;
-    cursor:pointer;
-    color: white !important;
-  }
-`
-export const NavMenu = styled.ul`
-  display:flex;
-  align-items:center;
-  justify-content:center;
-  list-style:none;
-  text-align:center;
-  margin-left:2rem;
+//   // margin-left: 3rem;
+// `
 
-  @media screen and (max-width:425px){
-    flex-direction:column;
-    width:100%;
-    height:60vh;
-    position:absolute;
-    top:80px;
-    top:${({click})=>(click?"100%":"-1000px")};
-    opacity:1;
-    margin-left:0;
-    margin-right:0;
-    transition: all 0.2s ease;
-    background:black;
-  }
-`
+// export const MobileIcon= styled.div`
+//   // display:none;
+//   font-size: 25px;
+//   position: absolute;
+//   right: 15px;
+//   top: 12px;
+// `
+// export const NavMenu = styled.ul`
+//   // align-items:center;
+//   border: 1px solid red;
+//   flex: 2;
+//   // flex-direction: column;
+//   // display:flex;
+//   // justify-content:center;
+//   // list-style:none;
+//   // margin: 0;
+//   text-align:center;
+//   // margin-left:2rem;
 
-export const NavItem=styled.li`
-  height:80px;
+//   // @media screen and (max-width:425px){
+//   //   flex-direction:column;
+//   //   width:100%;
+//   //   height:60vh;
+//   //   position:absolute;
+//   //   top:80px;
+//   //   top:${({click})=>(click?"100%":"-1000px")};
+//   //   opacity:1;
+//   //   margin-left:0;
+//   //   margin-right:0;
+//   //   transition: all 0.2s ease;
+//   //   // background:black;
+//   // }
+// `
 
-  @media screen and (max-width:425px){
-    width:100%;
-  }
-`
+// export const NavItem=styled.li`
+//   // height:80px;
 
-export const NavLinks=styled(Link)`
-  color:#8BEAFF;
-  display:flex;
-  text-decoration:none;
-  align-items:center;
-  justify-content:center;
-  padding-top:2rem;
-  padding-right:3rem;
-  &:hover{
-    color:white;
-    transform:translate(-2px);
-  }
-  @media screen and (max-width:425px){
-    width:100%;
-    align-items:center;
-  justify-content:center;
-  padding:0;
-}
+//   margin-top: calc(100vh / 12);
 
-`
-export const NavBtn=styled.div`
-  display:block;
-  align-items: center;
-  margin-right:0rem !important;
-  background-color:black;
-  color: #8BEAFF;
-  font-family: 'Manrope', sans-serif;
-  background:${({primary})=>(primary ? '#8BEAFF': 'transparent')};
-  padding:${({big})=>(big ? '16px 40px':'10px 32px')};
-  font-size:${({big})=>(big ? '20px':'16px')};
-  border:2px solid #8BEAFF;
-  cursor:pointer;
-  text-decoration:none;
-  transition: 0.3s !important;
-  border-radius:4px;
-  font-family: 'Manrope', sans-serif;
-  &:hover{
-    background:${({primary})=>(primary ? 'transparent': '#8BEAFF')};
-    color:black;
-    transform:translate(-2px);
-  }
-  @media screen and (max-width:425px){
-    margin-bottom:2rem;
-    width:50%;
-  }
-  @media screen and (max-width:768px){}
-  @media screen and (max-width:1024px){}
-`
+//   @media screen and (min-height: 350px){
+//     margin-top: calc((100vh) / 25);
+//   }
+
+//   @media screen and (min-height: 400px){
+//     margin-top: calc((100vh) / 19);
+//   }
+
+//   @media screen and (min-height: 525px){
+//     margin-top: calc((100vh) / 14);
+//   }
+
+//   @media screen and (min-height: 670px){
+//     margin-top: calc((100vh) / 9);
+//   }
+
+//   @media screen and (min-height: 880px){
+//     margin-top: calc((100vh) / 8);
+//   }
+// `
+
+// export const NavLinks=styled(Link)`
+//   color: #8BEAFF;
+//   font-size: 25px;
+//   text-decoration:none;
+//   &:hover{
+//     color:white;
+//   }
+
+//   // display:flex;
+//   // align-items:center;
+//   // justify-content:center;
+//   // padding-top:2rem;
+//   // padding-right:3rem;
+//   // @media screen and (max-width:425px){
+//   //   width:100%;
+//   //   align-items:center;
+//   // justify-content:center;
+//   // padding:0;
+// }
+
+// `
+// export const NavBtn=styled.div`
+//   align-items: center;
+//   border: 1px solid blue;
+//   padding: 42px 0;
+//   // padding:${({big})=>(big ? '16px 40px':'42px 0')};
+//   text-align: center;
+
+//   // display: none;
+//   // display:block;
+//   // margin-right:0rem !important;
+//   // background-color:black;
+//   // color: #8BEAFF;
+//   // font-family: 'Manrope', sans-serif;
+//   // background:${({primary})=>(primary ? '#8BEAFF': 'transparent')};
+
+//   // font-size:${({big})=>(big ? '20px':'16px')};
+//   // border:2px solid #8BEAFF;
+//   // cursor:pointer;
+//   // text-decoration:none;
+//   // transition: 0.3s !important;
+//   // border-radius:4px;
+//   // font-family: 'Manrope', sans-serif;
+//   // &:hover{
+//   //   background:${({primary})=>(primary ? 'transparent': '#8BEAFF')};
+//   //   color:black;
+//   //   transform:translate(-2px);
+//   // }
+//   // @media screen and (max-width:425px){
+//   //   margin-bottom:2rem;
+//   //   width:50%;
+//   // }
+//   // @media screen and (max-width:768px){}
+//   // @media screen and (max-width:1024px){}
+// `
