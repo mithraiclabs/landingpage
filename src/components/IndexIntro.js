eea3e28d3d656df7ec4c27d6d9e9532b1cc3d769
Tommy Johnson <tommyjohnson1028@gmail.com>
2022-01-04 10:59:19 -0800

    add simple TVL display

--- src/components/IndexIntro.js
+++ src/components/IndexIntro.js
@@ -1,10 +1,14 @@
-import "../styles/IndexIntro.scss";
-import React from "react";
-import RetroLogo from "../images/Retro logo2.png";
-import SolanaLogo from "../images/solana-logo-icon.svg";
-import SerumLogo from "../images/serum-logo-icon.svg";
+import "../styles/IndexIntro.scss"
+import React from "react"
+import RetroLogo from "../images/Retro logo2.png"
+import SolanaLogo from "../images/solana-logo-icon.svg"
+import SerumLogo from "../images/serum-logo-icon.svg"
+import useTvl from "../hooks/useTvl"
+import { formatLargeCurrency } from "../utils"
 
 const IndexIntro = () => {
+  const tvl = useTvl()
+
   return (
     <div className="index-intro">
       <div className="ii-announcement">
@@ -13,11 +17,18 @@ const IndexIntro = () => {
           <h2>European style options soon to follow</h2>
         </div>
         <div className="iia-button-group">
-          <a href="https://trade.psyoptions.io" className="p-button">Start trading</a>
-          <a href="https://docs.psyoptions.io" className="p-button">Read our docs</a>
+          <a href="https://trade.psyoptions.io" className="p-button">
+            Start trading
+          </a>
+          <a href="https://docs.psyoptions.io" className="p-button">
+            Read our docs
+          </a>
         </div>
         <div className="iia-first">
-          A first for blockchain. Winner of the <a href="https://solana.com/news/winners-of-the-solana-x-serum-defi-hackathon">Solana X Serum DeFi Hackathon</a>
+          A first for blockchain. Winner of the{" "}
+          <a href="https://solana.com/news/winners-of-the-solana-x-serum-defi-hackathon">
+            Solana X Serum DeFi Hackathon
+          </a>
         </div>
         <figure className="iia-built-with">
           <span>Built with Solana</span>&nbsp;
@@ -30,9 +41,14 @@ const IndexIntro = () => {
           </a>
         </figure>
       </div>
-      <img className="ii-retro-logo" src={RetroLogo} alt="PsyOptions retro logo with synthwave style"/>
+      {tvl && <div><h1>TVL: ${formatLargeCurrency(tvl)}</h1></div>}
+      <img
+        className="ii-retro-logo"
+        src={RetroLogo}
+        alt="PsyOptions retro logo with synthwave style"
+      />
     </div>
   )
 }
 
-export default IndexIntro;
+export default IndexIntro
