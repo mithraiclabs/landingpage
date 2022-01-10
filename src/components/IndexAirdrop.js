28d9d18069a95f13078dd3c0dc5b150c3b7a4cd4
XYZ <jasonhargrove@psyoptions.io>
2022-01-09 22:26:43 -0700

    add forum link, dapps, token page; styling & fixes

--- src/components/IndexAirdrop.js
+++ src/components/IndexAirdrop.js
@@ -1,6 +1,6 @@
 import "../styles/IndexAirdrop.scss";
 import React from "react";
-import Joystick from "../images/Joystick.png"
+import Joystick from "../images/Joystick.png";
 
 const IndexAirdrop = () => {
   return (
@@ -11,7 +11,7 @@ const IndexAirdrop = () => {
           Increase foundation funding and reduce sell pressure by airdropping options to your community. PsyOptions physically settled options can help align incentives with community and team members, while protecting the foundation from those who are only opportunistic
         </div>
       </div>
-      <img src ={Joystick} alt="PsyOptions Airdrops for communities, for example video game streamers"/> 
+      <img src={Joystick} alt="PsyOptions Airdrops for communities, for example video game streamers"/> 
     </div>
   );
 }
