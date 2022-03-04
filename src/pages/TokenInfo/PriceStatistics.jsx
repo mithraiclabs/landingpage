5b55ca8f112820d7337e6296a324433fa60807d4
Tommy Johnson <tommyjohnson1028@gmail.com>
2022-03-04 15:53:36 -0800

    add current PSY price

--- src/pages/TokenInfo/PriceStatistics.jsx
+++ src/pages/TokenInfo/PriceStatistics.jsx
@@ -1,39 +1,44 @@
-import React, { memo } from 'react';
-import { Box, Typography } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
-import {PRICE_STATISTICS, PSY_INITIAL_LAUNCH_PRICE, TOTAL_SUPPLY, CIRCULATING_SUPPLY} from "../../constants"
-import {theme} from "../../styles/theme"
+import React, { memo, useEffect } from "react"
+import { Box, Typography } from "@mui/material"
+import makeStyles from "@mui/styles/makeStyles"
+import usePsyPrice from "../../hooks/usePsyPrice"
+import {
+  PRICE_STATISTICS,
+  PSY_COINGECKO_PRICE,
+  TOTAL_SUPPLY,
+  CIRCULATING_SUPPLY,
+} from "../../constants"
+import { theme } from "../../styles/theme"
 import logo from "../../images/logo copy.png"
 
 const useStyles = makeStyles(() => ({
   titleContainer: {
-    fontFamily: 'Goldman',
-    fontWeight: '400',
-    marginBottom: '20px',
-    textAlign: 'center',
-    [theme.breakpoints.up('md')]: {
-      fontSize: '50px',
+    fontFamily: "Goldman",
+    fontWeight: "400",
+    marginBottom: "20px",
+    textAlign: "center",
+    [theme.breakpoints.up("md")]: {
+      fontSize: "50px",
     },
-    [theme.breakpoints.down('md')]: {
-      fontSize: '30px',
+    [theme.breakpoints.down("md")]: {
+      fontSize: "30px",
     },
   },
   heading: {
-    fontFamily: 'Goldman',
-    fontWeight: '400',
-    fontSize: '25px',
-    marginBottom: '20px',
+    fontFamily: "Goldman",
+    fontWeight: "400",
+    fontSize: "25px",
+    marginBottom: "20px",
   },
   title: {
-    lineHeight: '20px',
-    color: '#A39090',
+    lineHeight: "20px",
+    color: "#A39090",
   },
-}));
+}))
 
-
-
-const PriceStatistics= memo(() => {
-  const classes = useStyles();
+const PriceStatistics = memo(() => {
+  const classes = useStyles()
+  const psyPrice = usePsyPrice()
 
   return (
     <Box>
@@ -41,27 +46,21 @@ const PriceStatistics= memo(() => {
       <Box className={classes.heading}>{PRICE_STATISTICS}</Box>
 
       <Box py={2} textAlign="left">
-        <Typography className={classes.title}>
-          {PSY_INITIAL_LAUNCH_PRICE}
-        </Typography>
-        <Typography>$0.12</Typography>
+        <Typography className={classes.title}>{PSY_COINGECKO_PRICE}</Typography>
+        <Typography>${psyPrice?.toFixed(4)}</Typography>
       </Box>
 
       <Box py={2} textAlign="left">
-        <Typography className={classes.title}>
-          {TOTAL_SUPPLY}
-        </Typography>
+        <Typography className={classes.title}>{TOTAL_SUPPLY}</Typography>
         <Typography>1,000,000,000 PSY</Typography>
       </Box>
 
       <Box py={2} textAlign="left">
-        <Typography className={classes.title}>
-          {CIRCULATING_SUPPLY}
-        </Typography>
+        <Typography className={classes.title}>{CIRCULATING_SUPPLY}</Typography>
         <Typography>62,000,000 PSY</Typography>
       </Box>
     </Box>
-  );
-});
+  )
+})
 
-export default PriceStatistics;
+export default PriceStatistics
