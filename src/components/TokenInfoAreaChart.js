777f4d04fe9b138e5b3913a2f3df247238b7f73c
Tommy Johnson <tommyjohnson1028@gmail.com>
2022-03-04 15:34:44 -0800

    tweak styling of charts

--- src/components/TokenInfoAreaChart.js
+++ src/components/TokenInfoAreaChart.js
@@ -1,109 +1,150 @@
-import React from 'react';
-import { Chart, Series, ArgumentAxis, CommonSeriesSettings, Legend, Margin } from 'devextreme-react/chart';
-import { makeStyles } from '@mui/styles';
-import { Box } from '@mui/material';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import React from "react"
+import {
+  Chart,
+  Series,
+  ArgumentAxis,
+  CommonSeriesSettings,
+  Legend,
+  Margin,
+} from "devextreme-react/chart"
+import { makeStyles } from "@mui/styles"
+import { Box } from "@mui/material"
+import useMediaQuery from "@mui/material/useMediaQuery"
 
-import {ECONOMICS_LOCKUPS} from '../constants'
-import {theme} from "../styles/theme"
-
+import { ECONOMICS_LOCKUPS } from "../constants"
+import { theme } from "../styles/theme"
 
 const dataSource = [
-    {
-      year: "January 2022",
-      PrivateRound1: 5_000_000,
-      PrivateRound2: 7_000_000,
-      IEO: 30_000_000,
-      PSYLiquidityProvisions: 20_000_000,
-      DAOTreasury: 600_000_000,
-    },
-    {
-      year: "January 2023",
-      CoreTeamAdvisors: 37_500_000,
-      PrivateRound1: 16_250_000,
-      PrivateRound2: 22_750_000,
-      ContributorFund: 20_000_000,
-      IEO: 30_000_000,
-      PSYLiquidityProvisions: 20_000_000,
-      DAOTreasury: 600_000_000,
-    },
-    {
-      year: "January 2024",
-      CoreTeamAdvisors: 75_000_000,
-      PrivateRound1: 27_500_000,
-      PrivateRound2: 38_500_000,
-      ContributorFund: 40_000_000,
-      IEO: 30_000_000,
-      PSYLiquidityProvisions: 20_000_000,
-      DAOTreasury: 600_000_000,
-    },
-    {
-      year: "January 2025",
-      CoreTeamAdvisors: 112_000_000,
-      PrivateRound1: 38_750_000,
-      PrivateRound2: 54_250_000,
-      ContributorFund: 60_000_000,
-      IEO: 30_000_000,
-      PSYLiquidityProvisions: 20_000_000,
-      DAOTreasury: 600_000_000,
-    },
-    {
-      year: "January 2026",
-      CoreTeamAdvisors: 150_000_000,
-      PrivateRound1: 50_000_000,
-      PrivateRound2: 70_000_000,
-      ContributorFund: 80_000_000,
-      IEO: 30_000_000,
-      PSYLiquidityProvisions: 20_000_000,
-      DAOTreasury: 600_000_000,
-    },
-
-  ];
+  {
+    year: "January 2022",
+    PrivateRoundTGE: 12_000_000,
+    IEO: 30_000_000,
+    PSYLiquidityProvisions: 20_000_000,
+    DAOTreasury: 600_000_000,
+  },
+  {
+    year: "January 2023",
+    CoreTeamAdvisors: 37_500_000,
+    PrivateRoundTGE: 12_000_000,
+    PrivateRounds: 27_000_000,
+    ContributorFund: 20_000_000,
+    IEO: 30_000_000,
+    PSYLiquidityProvisions: 20_000_000,
+    DAOTreasury: 600_000_000,
+  },
+  {
+    year: "January 2024",
+    CoreTeamAdvisors: 75_000_000,
+    PrivateRoundTGE: 12_000_000,
+    PrivateRounds: 54_000_000,
+    ContributorFund: 40_000_000,
+    IEO: 30_000_000,
+    PSYLiquidityProvisions: 20_000_000,
+    DAOTreasury: 600_000_000,
+  },
+  {
+    year: "January 2025",
+    CoreTeamAdvisors: 112_000_000,
+    PrivateRoundTGE: 12_000_000,
+    PrivateRounds: 81_000_000,
+    ContributorFund: 60_000_000,
+    IEO: 30_000_000,
+    PSYLiquidityProvisions: 20_000_000,
+    DAOTreasury: 600_000_000,
+  },
+  {
+    year: "January 2026",
+    CoreTeamAdvisors: 150_000_000,
+    PrivateRoundTGE: 12_000_000,
+    PrivateRounds: 108_000_000,
+    ContributorFund: 80_000_000,
+    IEO: 30_000_000,
+    PSYLiquidityProvisions: 20_000_000,
+    DAOTreasury: 600_000_000,
+  },
+]
 
 const useStyles = makeStyles(() => ({
   heading: {
-    fontFamily: 'Goldman',
-    fontWeight: '400',
-    marginLeft: '7px',
-    color: 'white',
-    margin: '30px 0',
-    [theme.breakpoints.up('md')]:{
-      fontSize: '40px',
+    fontFamily: "Goldman",
+    fontWeight: "400",
+    marginLeft: "7px",
+    color: "white",
+    margin: "30px 0",
+    [theme.breakpoints.up("md")]: {
+      fontSize: "40px",
     },
-    [theme.breakpoints.down('md')]:{
-      fontSize: '30px',
-    }
+    [theme.breakpoints.down("md")]: {
+      fontSize: "30px",
+    },
   },
-}));
-
+}))
 
 const DevAreChart = () => {
-  const classes = useStyles();
-  const isDesktop = useMediaQuery('(min-width:1200px)');
+  const classes = useStyles()
+  const isDesktop = useMediaQuery("(min-width:1200px)")
 
   return (
-    <Box sx={{color:'white !important'}}>
-      <Box
-        className={classes.heading}
+    <Box sx={{ color: "white !important" }}>
+      <Box className={classes.heading}>{ECONOMICS_LOCKUPS}</Box>
+      <Chart
+        axis
+        color="white"
+        commonAxisSettings={{
+          grid: { visible: false },
+          label: { font: { size: 16, color: "white" } },
+        }}
+        palette="dark"
+        dataSource={dataSource}
       >
-        {ECONOMICS_LOCKUPS}
-      </Box>
-      <Chart axis color='white' commonAxisSettings={{grid:{visible:false}, label:{font:{color:'white'}}}} palette="dark" dataSource={dataSource}>
-        <CommonSeriesSettings color='white' argumentField="year" type="stackedarea" valueField="DAOTreasury" />
-        <Series  CommonSeriesSettings={{}} color="#677BDB" valueField="DAOTreasury" name="DAO Treasury"></Series>
-        <Series color='#8B24C9' valueField="IEO" name="IEO"></Series>
-        <Series color='#D167EB' valueField="PSYLiquidityProvisions" name="PSY Liquidity Provisions"></Series>
-        <Series color="#AA24B3" valueField="PrivateRound1" name="Private Round 1"></Series>
-        <Series color='#DB2FFA'  valueField="PrivateRound2" name="Private Round 2"></Series>
-        <Series color='#FE06C3' valueField="ContributorFund" name="Contributor Fund"></Series>
-        <Series color="#E48DFF" valueField="CoreTeamAdvisors" name="Core Team & Advisors"></Series>
+        <CommonSeriesSettings
+          color="white"
+          argumentField="year"
+          type="stackedarea"
+          valueField="DAOTreasury"
+        />
+        <Series
+          color="#677BDB"
+          valueField="DAOTreasury"
+          name="DAO Treasury"
+        ></Series>
+        <Series color="#8B24C9" valueField="IEO" name="IEO"></Series>
+        <Series
+          color="#D167EB"
+          valueField="PSYLiquidityProvisions"
+          name="PSY Liquidity Provisions"
+        ></Series>
+        <Series
+          color="#DB2FFA"
+          valueField="PrivateRoundTGE"
+          name="Private Rounds TGE"
+        ></Series>
+        <Series
+          color="#AA24B3"
+          valueField="PrivateRounds"
+          name="Private Rounds"
+        ></Series>
+        <Series
+          color="#FE06C3"
+          valueField="ContributorFund"
+          name="Contributor Fund"
+        ></Series>
+        <Series
+          color="#E48DFF"
+          valueField="CoreTeamAdvisors"
+          name="Core Team & Advisors"
+        ></Series>
 
         <Margin bottom={20} />
         <ArgumentAxis valueMarginsEnabled={false} />
-        <Legend verticalAlignment={isDesktop ? "top" : 'bottom' } horizontalAlignment={isDesktop? "right" : 'center' } />
+        <Legend
+          font={{ size: 16 }}
+          verticalAlignment={isDesktop ? "top" : "bottom"}
+          horizontalAlignment={isDesktop ? "right" : "center"}
+        />
       </Chart>
     </Box>
-  );
-};
+  )
+}
 
-export default DevAreChart;
+export default DevAreChart
--- src/components/TokenInfoPieChart.js
+++ src/components/TokenInfoPieChart.js
@@ -1,90 +1,106 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import PieChart, { Series, Label, Margin, Legend, Animation } from 'devextreme-react/pie-chart';
-import { makeStyles } from '@mui/styles';
-import { PSY_TOKEN_SUPPLY } from '../constants';
-import {theme} from "../styles/theme"
+import React from "react"
+import { Box } from "@mui/material"
+import PieChart, {
+  Series,
+  Label,
+  Margin,
+  Legend,
+  Animation,
+} from "devextreme-react/pie-chart"
+import { makeStyles } from "@mui/styles"
+import { PSY_TOKEN_SUPPLY } from "../constants"
+import { theme } from "../styles/theme"
 
 const dataSource = [
-    {
-      country: "DAO Treasury",
-      medals: 60
-    },
-    {
-      country: "Contributor Fund",
-      medals: 8
-    },
-    {
-      country: "PSY Liquidity Provisions",
-      medals: 2
-    },
-    {
-      country: "Initial Exchange Offering",
-      medals: 3
-    },
-    {
-      country: "Private Round 2",
-      medals: 7
-    },
-    {
-      country: "Private Round 1",
-      medals: 5
-    },
-    {
-      country: "Core Team & Advisors",
-      medals: 15
-    }
-  ];
-  
+  {
+    group: "DAO Treasury",
+    allocation: 60,
+  },
+  {
+    group: "Contributor Fund",
+    allocation: 8,
+  },
+  {
+    group: "PSY Liquidity Provisions",
+    allocation: 2,
+  },
+  {
+    group: "Initial Exchange Offering",
+    allocation: 3,
+  },
+  {
+    group: "Private Round 2",
+    allocation: 7,
+  },
+  {
+    group: "Private Round 1",
+    allocation: 5,
+  },
+  {
+    group: "Core Team & Advisors",
+    allocation: 15,
+  },
+]
 
 const useStyles = makeStyles(() => ({
   heading: {
-    fontFamily: 'Goldman',
-    fontWeight: '400',
-    marginLeft: '7px',
-    color: 'white',
-    margin: '30px 0',
-    textAlign: 'center',
-    [theme.breakpoints.up('md')]: {
-      fontSize: '40px',
+    fontFamily: "Goldman",
+    fontWeight: "400",
+    marginLeft: "7px",
+    color: "white",
+    margin: "30px 0",
+    textAlign: "center",
+    [theme.breakpoints.up("md")]: {
+      fontSize: "40px",
     },
-    [theme.breakpoints.down('md')]: {
-      fontSize: '30px',
+    [theme.breakpoints.down("md")]: {
+      fontSize: "30px",
     },
   },
-}));
+}))
 
-const customizePoint = (pointInfo) => {
+const customizePoint = pointInfo => {
   switch (pointInfo.argument) {
-    case 'DAO Treasury':
-      return { color: '#ff3ceb' };
-    case 'Contributor Fund':
-      return { color: '#560BAD' };
-    case 'PSY Liquidity Provisions':
-      return { color: '#F100AE' };
-    case 'Initial Exchange Offering':
-      return { color: '#FF3CEB' };
-    case 'Private Round 2':
-      return { color: '#B600F1' };
-    case 'Private Round 1':
-      return { color: '#FF3CEB' };
+    case "DAO Treasury":
+      return { color: "#ff3ceb" }
+    case "Contributor Fund":
+      return { color: "#560BAD" }
+    case "PSY Liquidity Provisions":
+      return { color: "#F100AE" }
+    case "Initial Exchange Offering":
+      return { color: "#FF3CEB" }
+    case "Private Round 2":
+      return { color: "#B600F1" }
+    case "Private Round 1":
+      return { color: "#FF3CEB" }
     case `Core Team & Advisors`:
-      return { color: '#EAA9FF' };
+      return { color: "#EAA9FF" }
     default:
-      return {};
+      return {}
   }
-};
+}
 
 const TokenInfoPieChart = () => {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <React.Fragment>
-      <Box sx={{ marginBottom: { sm: '20px', md: '50px' } }}>
+      <Box sx={{ marginBottom: { sm: "20px", md: "50px" } }}>
         <Box className={classes.heading}>{PSY_TOKEN_SUPPLY}</Box>
-        <PieChart type="doughnut" innerRadius={0.7} dataSource={dataSource} customizePoint={customizePoint}>
-          <Series argumentField="country" valueField="medals">
-            <Label backgroundColor="black" visible={true} customizeText={formatText} />
+        <PieChart
+          type="doughnut"
+          innerRadius={0.7}
+          dataSource={dataSource}
+          customizePoint={customizePoint}
+          resolveLabelOverlapping="shift"
+        >
+          <Series argumentField="group" valueField="allocation">
+            <Label
+              backgroundColor="black"
+              visible={true}
+              customizeText={formatText}
+              font={{ size: 16 }}
+            />
           </Series>
           <Margin bottom={20} />
           <Legend visible={false} />
@@ -92,11 +108,11 @@ const TokenInfoPieChart = () => {
         </PieChart>
       </Box>
     </React.Fragment>
-  );
-};
-
-function formatText(arg) {
-  return `${arg.argumentText} (${arg.percentText})`;
+  )
 }
 
-export default TokenInfoPieChart;
+function formatText(arg) {
+  return `${arg.argumentText} (${arg.percentText})`
+}
+
+export default TokenInfoPieChart
